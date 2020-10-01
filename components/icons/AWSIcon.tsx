fe9a10b5855e33a64a1214ced93b420df123cf14
marcomcnultycodes <marcomcnultycodes@gmail.com>
2020-10-01 20:54:54 +0100

    👌 IMPROVE: fix broken TS icon

--- components/icons/AWSIcon.tsx
+++ components/icons/AWSIcon.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent as FC } from 'react';
 import iIconProps from '../../models/iIconProps';
 
-const AWSIcon: FC<iIconProps> = ({ children, width, height }) => (
+const AWSIcon: FC<iIconProps> = ({ width, height }) => (
   <svg
     viewBox="0 0 128 128"
     version="1"
